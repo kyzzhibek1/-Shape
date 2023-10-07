f26474712e01fcd3196ce9b6ebf81b1a4b04cc11
jibek <120552319+kyzzhibek1@users.noreply.github.com>
2023-10-07 19:20:11 +0600

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,33 @@
+public class Circle extends Shape {
+    private double radius;
+
+
+    public Circle(double r) {
+        super("Circle");
+        this.radius = r;
+    }
+
+    public double getArea() {
+        return Math.PI * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+public class Circle extends Shape {
+    private double radius;
+
+
+    public Circle(double r) {
+        super("Circle");
+        this.radius = r;
+    }
+
+    public double getArea() {
+        return Math.PI * radius;
+    }
+
+    public double getPerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
